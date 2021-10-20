a348d38432c938d9abf3e7caadab042d3937a2c8
Elbi123 <elbetelworkineh59@gmail.com>
2021-10-20 10:37:24 +0300

    working with auth routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,26 +1,36 @@
 const express = require("express");
-const { verifySignup } = require("./../middleware/index");
 const controller = require("./../controller/auth.controller");
+const { verifySignup } = require("./../middleware/index");
+const inputValidation = require("./../middleware/inputValidation");
+const verifySignIn = require("./../middleware/verifySignIn");
 
-const app = express();
+const router = express.Router();
 
-app.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
+router
+    .route("/signup")
+    .post(
+        [
+            inputValidation.checkEmptyFields,
+            inputValidation.nameValidation,
+            inputValidation.passwordValidation,
+            inputValidation.phoneNumberAndCountryCode,
+            verifySignup.checkDuplicateUsernameOrPhoneNumber,
+            verifySignup.checkRoleExisted,
+        ],
+        controller.signup
     );
-    next();
-});
 
-app.post(
-    "/api/auth/signup",
-    [
-        verifySignup.checkDuplicateUsernameOrPhoneNumber,
-        verifySignup.checkRoleExisted,
-    ],
-    controller.signup
-);
+router.route("/verify/:phoneNumber").post(controller.verify);
 
-app.post("/api/auth/signin", [controller.signin]);
+router
+    .route("/signin")
+    .post(
+        [
+            inputValidation.checkIfPhoneNumberFieldIsEmpty,
+            inputValidation.phoneNumberAndCountryCode,
+            verifySignIn.checkIfAccountIsVerified,
+        ],
+        controller.signin
+    );
 
-module.exports = app;
+module.exports = router;

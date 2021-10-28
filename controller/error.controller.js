77dd5e657bd9175de03768429b22f25a920a38fc
Elbi123 <elbetelworkineh59@gmail.com>
2021-10-28 13:27:20 +0300

    working with error controller

--- controller/error.controller.js
+++ controller/error.controller.js
@@ -1,3 +1,16 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateKeyErrorDB = (err) => {
+    const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
+    const message = `Duplicate field value: ${value[0]}. Please use another value!`;
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -29,14 +42,16 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
-    if (process.env.NODE_ENV === "development") {
-        sendErrorDev(err, res);
-    } else if (process.env.NODE_ENV === "production") {
-        sendErrorProd(err, res);
-    }
+    let error = { ...err };
+    error.name = err.name || "";
+    error.message = err.message || "";
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(error, res);
+    } else if (process.env.NODE_ENV === "production") {
+        if (error.name === "CastError") error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateKeyErrorDB(error);
+
+        sendErrorProd(error, res);
+    }
 };
